6ab359b80686cbae0f13a815658711c27af23cbe
@p_craft <ef81sp@gmail.com>
2019-11-27 23:36:53 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const gpio = require('rpi-gpio');
+const PIN = 12;
+// gpio.setMode(gpio.MODE_BCM);
+let status = false;
+let isReady = false;
+
+const log = (...txt) => {
+  console.log(new Date().toString(), txt);
+};
+
+gpio.on('change', (ch, value) => {
+  log(ch, value);
+  status = value;
+});
+
+gpio.setup(PIN, gpio.DIR_IN, gpio.EDGE_BOTH, () => {
+  console.log('RaspberryPi GPIO is ready!');
+  isReady = true;
+});
+
+// ==================
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => console.log('express is ready!'));
+
+app.get('/status', (req, res) => {
+  if (!isReady) return res.status(500).json({ message: 'not ready' });
+  const response = { isOccupied: status };
+  log(response);
+  return res.json(response);
+});
